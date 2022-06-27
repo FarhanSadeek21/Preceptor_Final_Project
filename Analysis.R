64c214d3bd600cf4d74c26d29f357f4b0548a851
FarhanSadeek21 <farhansadeek019@gmail.org>
2022-06-27 19:04:49 -0400

    Commit to github

--- Analysis.R
+++ Analysis.R
@@ -1,6 +1,36 @@
-us_state <- read_csv('US States and Cost of Living.csv')
+library(tidyverse)
+library(ggplotly)
+data <- read.csv("data.csv")
 
-us_state |>
-    select(GDP Per Capita 2019, State) |>
-    ggplot(aes(State, GDP Per Capita 2019))+
-    geom_bar()
+data |>
+    drop_na() |>
+    select(Continent, Cost.of.Living.Index, Local.Purchasing.Power.Index, Country) |>
+    ggplot(aes(Local.Purchasing.Power.Index, Cost.of.Living.Index, color=Continent, size = 50))+
+    geom_point()+
+    facet_wrap(~Continent)
+
+csvData <- read.csv("csvData.csv")
+View(csvData)
+data |>
+    drop_na() |>
+    select(Continent, Cost.of.Living.Index, Local.Purchasing.Power.Index, Country) |>
+    ggplot(aes(Local.Purchasing.Power.Index, Cost.of.Living.Index, color=Continent))+
+    labs(title="Cost of Living and Local Purchasing Power Index of Countries around the world",
+         subtitle="Richer countries have both higher cost of living and purchasing power",
+         x="Local Purchasing Power Index",
+         y="Cost of Living Index",
+         caption = "Source of Data : Numbeo.com")+
+    geom_point()
+
+csvData |>
+    drop_na() |>
+    select(Density, GrowthRate, pop2020, pop2022, continent) |>
+    ggplot(aes(GrowthRate, Density, color=continent, size = pop2022))+
+    geom_point()
+
+
+csvData |>
+    # drop_na() |>
+    #select(Density, GrowthRate, pop2020, pop2022, continent) |>
+    ggplot(mapping = aes(x = pop2022, y = area, color=continent))+
+    geom_point()
